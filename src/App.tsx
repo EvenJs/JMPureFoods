06f70a8be4112e333b66c377063b04cf9b4d047a
Johnson Wang <yuhang0229@gmail.com>
2026-04-26 16:30:14 +1000

    feat: page rapper for animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import PageWrapper from "./components/ui/PageWrapper";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import FoodService from "./pages/FoodService";
 import ContactUs from "./pages/ContactUs";
-import Footer from "./components/layout/Footer";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
       <Navbar />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/food-service" element={<FoodService />} />
-          <Route path="/contact" element={<ContactUs />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <PageWrapper>
+                  <Home />
+                </PageWrapper>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <PageWrapper>
+                  <About />
+                </PageWrapper>
+              }
+            />
+            <Route
+              path="/services"
+              element={
+                <PageWrapper>
+                  <Services />
+                </PageWrapper>
+              }
+            />
+            <Route
+              path="/food-service"
+              element={
+                <PageWrapper>
+                  <FoodService />
+                </PageWrapper>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <PageWrapper>
+                  <ContactUs />
+                </PageWrapper>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </main>
       <Footer />
     </>
